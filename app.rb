c615ff459693b6e90fc4f078b1a9058e0757644d
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-08 23:10:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     @text = params[:user_phrase]
-    @piglatinizer = PigLatinizer.new
-    binding.pry
+    @piglatinizer = PigLatinizer.new(@text)
+      # binding.pry
     erb :piglatinize
   end
 
